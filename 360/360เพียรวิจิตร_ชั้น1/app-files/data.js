729f740ba0bb3631f91a79929b324fe1037b5fbe
fluke1577 <anuchawonglakorn1577@gmail.com>
2024-10-14 00:49:44 +0700

    WEB360

---
+++ 360/360เพียรวิจิตร_ชั้น1/app-files/data.js
@@ -0,0 +1,263 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "หน้าตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.684297908300862,
+          "pitch": 0.10435352205561621,
+          "rotation": 0,
+          "target": "1--1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--1-",
+      "name": "ชั้นที่ 1 ตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.07704543619246174,
+        "pitch": -0.020030686896699734,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12611728062401006,
+          "pitch": 0.08505595471284266,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.8472643581685197,
+          "pitch": 0.13755888117272264,
+          "rotation": 0,
+          "target": "2--1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--1-",
+      "name": "ชั้นที่ 1 ตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6608114476210085,
+          "pitch": 0.12893596819382402,
+          "rotation": 0,
+          "target": "1--1-"
+        },
+        {
+          "yaw": 3.076432190171924,
+          "pitch": 0.12155219181696886,
+          "rotation": 0,
+          "target": "5--1-"
+        },
+        {
+          "yaw": -1.4665142639324849,
+          "pitch": 0.05140028817997866,
+          "rotation": 0.7853981633974483,
+          "target": "3--1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--1-",
+      "name": "ชั้นที่ 1 ตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.543540286619028,
+          "pitch": 0.10709449166087381,
+          "rotation": 5.497787143782138,
+          "target": "2--1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--1-",
+      "name": "ชั้นที่ 1 ตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.138267405440951,
+          "pitch": 0.16227628640232084,
+          "rotation": 0,
+          "target": "5--1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--1-",
+      "name": "ชั้นที่ 1 ตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6563242352165801,
+          "pitch": 0.15887405594947168,
+          "rotation": 0,
+          "target": "2--1-"
+        },
+        {
+          "yaw": 1.8399130534934391,
+          "pitch": 0.12605560645660852,
+          "rotation": 0,
+          "target": "4--1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ 360/360เพียรวิจิตร_ชั้น2/app-files/data.js
@@ -0,0 +1,659 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--2-",
+      "name": "ชั้นที่ 2 ตึกเพียรวิจิตร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6301221081987318,
+          "pitch": 0.07812575188992454,
+          "rotation": 0,
+          "target": "1--2"
+        },
+        {
+          "yaw": 0.1496999125251932,
+          "pitch": 0.0044707934954608675,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.5345751744313922,
+          "pitch": 0.012672817957687599,
+          "rotation": 5.497787143782138,
+          "target": "4---"
+        },
+        {
+          "yaw": -3.0712617135778757,
+          "pitch": 0.10317712968264203,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2",
+      "name": "ระเบียงชั้นที่ 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4160540144840468,
+          "pitch": 0.022227649154380913,
+          "rotation": 0,
+          "target": "0--2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "สโมสรนักศึกษาคณะวิศวกรรมศาสตร์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.914683839575,
+          "pitch": 0.12764649985204812,
+          "rotation": 0,
+          "target": "0--2-"
+        },
+        {
+          "yaw": 1.3349345317856294,
+          "pitch": 0.07529795382119175,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "สโมสรนักศึกษาคณะวิศวกรรมศาสตร์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3175973483382215,
+          "pitch": 0.050235589184037366,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "ฝ่ายวิจัย บัณฑิตศึกษา และอุตสาหกรรมสัมพันธ์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.16149809409571603,
+          "pitch": 0.05535088927041798,
+          "rotation": 0.7853981633974483,
+          "target": "0--2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "ทางเดินศูนย์เรียนรู้ดิจิตอลสร้างสรรค์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14308844996329206,
+          "pitch": 0.028254302659519226,
+          "rotation": 0,
+          "target": "0--2-"
+        },
+        {
+          "yaw": 3.0883343683029523,
+          "pitch": 0.06334654571637088,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "ศูนย์เรียนรู้ดิจิตอลสร้างสรรค์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9986138149686674,
+          "pitch": 0.1378971330776455,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.12985679074022727,
+          "pitch": 0.18918070377030993,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -0.46190045652611644,
+          "pitch": 0.2154713391421197,
+          "rotation": 5.497787143782138,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.7814751667608029,
+          "pitch": 0.23278959424055046,
+          "rotation": 1.5707963267948966,
+          "target": "9-smart-classroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "ศูนย์เรียนรู้ดิจิตอลสร้างสรรค์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3318853563948743,
+          "pitch": 0.03406764118346217,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 1.5509642274727353,
+          "pitch": 0.14439151752890922,
+          "rotation": 0,
+          "target": "14-video-recording-studio"
+        },
+        {
+          "yaw": 2.465436187284446,
+          "pitch": 0.1504722966583376,
+          "rotation": 1.5707963267948966,
+          "target": "11-boardcast--virtual-studio"
+        },
+        {
+          "yaw": 2.1239045094126086,
+          "pitch": 0.12132334847790105,
+          "rotation": 0,
+          "target": "13-metaverse-studio"
+        },
+        {
+          "yaw": 2.272303037097161,
+          "pitch": 0.13219100850036725,
+          "rotation": 0.7853981633974483,
+          "target": "12-recording-studio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "ศูนย์เรียนรู้ดิจิตอลสร้างสรรค์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3968673721105063,
+          "pitch": 0.23700272644936682,
+          "rotation": 5.497787143782138,
+          "target": "6-"
+        },
+        {
+          "yaw": -0.21007601371359108,
+          "pitch": 0.12662856168531,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-smart-classroom",
+      "name": "Smart Classroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1098645055988747,
+          "pitch": 0.20162666958380626,
+          "rotation": 1.5707963267948966,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "ศูนย์เรียนรู้ดิจิตอลสร้างสรรค์",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0996397362427706,
+          "pitch": 0.18037968469720234,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-boardcast--virtual-studio",
+      "name": "Boardcast & Virtual Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5880419164359658,
+          "pitch": 0.03789684820245398,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-recording-studio",
+      "name": "Recording Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7527249566541201,
+          "pitch": 0.01696217197366856,
+          "rotation": 0.7853981633974483,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-metaverse-studio",
+      "name": "Metaverse Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5293445243253387,
+          "pitch": 0.0682638378246363,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-video-recording-studio",
+      "name": "Video Recording Studio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4503760422486351,
+          "pitch": 0.008160393444384795,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
